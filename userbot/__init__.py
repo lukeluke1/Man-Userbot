2c642a2a57c16e4a62849262e7489e736bdf94d5
lukeluke1 <98029326+lukeluke1@users.noreply.github.com>
2022-01-19 19:53:06 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -155,7 +155,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/mrismanaziz/Man-Userbot.git"
+    "UPSTREAM_REPO_URL", "https://github.com/lukeluke1/Man-Userbot.git"
 )
 
 # SQL Database URI
